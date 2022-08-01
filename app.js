cbc810b88e9de2259c4595fe3f264c8e8b01a92b
Harshul Rai <harshuljps@gmail.com>
2022-08-02 02:28:33 +0530

    Task Booking

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -5,14 +7,17 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/success');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
+app.use(success);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(4000);
