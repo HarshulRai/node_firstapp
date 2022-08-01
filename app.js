190ca379e1e3709e7a22a0c0ba03782067445748
Harshul Rai <harshuljps@gmail.com>
2022-08-01 21:55:03 +0530

    Express-1

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const http = require('http');
 
-const routes = require('./routes');
 
-console.log(routes.someText);
+const express = require('express');
 
-const server = http.createServer(routes.handler);  
-  
-server.listen(4000);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); //Allows the request to continue in the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In the other middleware');
+    res.send('<h1>Hellofrom Express</h1>'); // This is to send response of any body type
+});
+
+app.listen(4000); 
