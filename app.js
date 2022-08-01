113a9b0bcf9f95c697fba84a264054a37a867468
Harshul Rai <harshuljps@gmail.com>
2022-08-01 13:12:19 +0530

    8th task

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 const http = require('http');
+const fs = require('fs');
 
 
-// const server = http.createServer((req, res) => {
-    
-//     res.setHeader('Content-Type', 'text/html');
-//     res.write('<html>');
-//     res.write('<head><title> My First page</title></head');
-//     res.write('<body><h1>Hello from my Node.js Server</h1></body>');
-//     res.write('</html>');
-//     res.end();
-// });
-
-// server.listen(4000);
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
+    
 
-    if(url === '/home') {
-        res.setHeader('Content-Type', 'text/html');
+    if(url === '/') {
+        
         res.write('<html>');
-        res.write('<head><title> My First page</title></head');
-        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('<head><title> Enter Message</title></head');
+        const read = fs.readFileSync('message.txt', function(error, response){
+            if(error){
+                res.writeHead(404);
+            }
+            else{
+                res.writeHead(200, { 'Content-Type': 'text/html'});
+                res.write(response);
+            }
+        })
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        res.end();
+        return res.end();
     }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+            
+        });
+        
 
-    if(url === '/about') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> My First page</title></head');
-        res.write('<body><h1>Welcome to about US Page</h1></body>');
-        res.write('</html>');
-        res.end();
     }
-
-    if(url === '/node') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> My First page</title></head');
-        res.write('<body><h1>Welcome to my Node.js Project</h1></body>');
-        res.write('</html>');
-        res.end();
-    }
-
-    else {
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<html>');
-            res.write('<head><title> My First page</title></head');
-            res.write('<body><h1>Hello from my Node.js Server</h1></body>');
-            res.write('</html>');
-            res.end();
-    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> My First page</title></head');
+    res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+    res.write('</html>');
+    res.end();
+   
    
 });
 
