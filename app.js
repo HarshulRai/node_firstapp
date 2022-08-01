972a9f3c84051848ed9f767b6353be0dbea4ffa6
Harshul Rai <harshuljps@gmail.com>
2022-08-01 23:09:29 +0530

    Express-3

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {    
-    res.send('<form action="/product" method="POST"><input type="text" name="title">Add Product</form><br><form action="/product" method="POST"><input type="number" name="size">Add Size<button type="submit">Submit</button></form>'); 
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not found</h1>');
 });
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {   
-    res.send('<h1>Hello from Express</h1>'); 
-});
-
-app.listen(4000); 
+app.listen(4000);
